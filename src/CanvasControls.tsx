337366f719f68fa5f224581152803452f0a083e9
Waqas Abbasi <wa.aliabbasi@gmail.com>
2021-11-28 13:51:04 +0100

    Add responsiveness to CanvasControls

--- src/CanvasControls.tsx
+++ src/CanvasControls.tsx
@@ -2,9 +2,8 @@ import React, {ChangeEvent} from "react";
 import {DrawingTool} from "./App";
 import Draggable from 'react-draggable';
 import './CanvasControls.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen, faEraser, faFillDrip, faSave} from '@fortawesome/free-solid-svg-icons'
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faPen, faEraser, faFillDrip, faSave} from '@fortawesome/free-solid-svg-icons'
 
 
 const COLORS = ['#FFFFFF', '#000000', '#FF0000', '#00FF00', '#0000FF',
@@ -39,36 +38,41 @@ export default function CanvasControl(
     return (
         <Draggable bounds="parent" cancel=".cancel" nodeRef={nodeRef}>
             <div className={"canvas-setting  no-hover"} ref={nodeRef}>
-                <div className={'button-icon'}>
-                    <div className="predefined-color no-hover" style={{background: color}}/>
+                <div className={'canvas-setting-left'}>
+                    <div className={'button-icon'}>
+                        <div className="predefined-color no-hover" style={{background: color}}/>
+                    </div>
+                    <div className={'button-icon'} onClick={() => updateCurrentTool(DrawingTool.Pen)}>
+                        <FontAwesomeIcon icon={faPen}/>
+                        <p className={'button-text'}>Pen</p>
+                    </div>
+                    <div className={'button-icon no-hover'}>
+                        <div className="value">{lineWidth}</div>
+                        <input type="range" min="10" max="50" onChange={updateLineWidth} value={lineWidth}
+                               className="slider cancel"/>
+                    </div>
+                    <div className={'button-icon'} onClick={() => updateCurrentTool(DrawingTool.Eraser)}>
+                        <FontAwesomeIcon icon={faEraser}/>
+                        <p className={'button-text'}>Eraser</p>
+                    </div>
+                    <div className={'button-icon'} onClick={clearCanvas}>
+                        <p className={'button-text'}>Clear</p>
+                    </div>
+                    <div className={'button-icon'} onClick={() => updateCurrentTool(DrawingTool.Fill)}>
+                        <FontAwesomeIcon icon={faFillDrip}/>
+                        <p className={'button-text'}>Fill</p>
+                    </div>
                 </div>
-                <div className={'button-icon'} onClick={() => updateCurrentTool(DrawingTool.Pen)}>
-                    <FontAwesomeIcon icon={faPen}  />
-                    <p className={'button-text'}>Pen</p>
-                </div>
-                <div className={'button-icon no-hover'}>
-                    <div className="value">{lineWidth}</div>
-                    <input type="range" min="10" max="50" onChange={updateLineWidth} value={lineWidth}
-                           className="slider cancel"/>
-                </div>
-                <div className={'button-icon'} onClick={() => updateCurrentTool(DrawingTool.Eraser)}>
-                    <FontAwesomeIcon icon={faEraser}  />
-                    <p className={'button-text'}>Eraser</p>
-                </div>
-                <div className={'button-icon'} onClick={clearCanvas}>
-                    <p className={'button-text'}>Clear</p>
-                </div>
-                <div className={'button-icon'} onClick={() => updateCurrentTool(DrawingTool.Fill)}>
-                    <FontAwesomeIcon icon={faFillDrip}  />
-                    <p className={'button-text'}>Fill</p>
-                </div>
-                <div className={'button-icon no-hover'}>
-                {COLORS.map(color => <div key={color} className="predefined-color hover-opacity" style={{background: color}}
-                                          onClick={() => updateColor(color)}/>)}
-                </div>
-                <div className={'button-icon'} onClick={saveImage}>
-                    <FontAwesomeIcon icon={faSave}  />
-                    <p className={'button-text'}>Save</p>
+                <div className={'canvas-setting-right'}>
+                    <div className={'button-icon no-hover'}>
+                        {COLORS.map(color => <div key={color} className="predefined-color hover-opacity"
+                                                  style={{background: color}}
+                                                  onClick={() => updateColor(color)}/>)}
+                    </div>
+                    <div className={'button-icon'} onClick={saveImage}>
+                        <FontAwesomeIcon icon={faSave}/>
+                        <p className={'button-text'}>Save</p>
+                    </div>
                 </div>
             </div>
         </Draggable>
